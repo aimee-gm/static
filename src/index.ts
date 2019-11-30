71cca0399a72affbbee82fc1617dd79dda9f7c31
aimee.gm <aimee@aimes.me.uk>
2019-11-30 12:56:11 +0000

    export useLocation

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { FileData } from './types';
 export { loadYamlData } from './data/yaml';
 export { loadFrontMatter } from './data/font-matter';
 export { ContentGenerator, register, registerAll, registerOne, render } from './generator';
-export { renderReact } from './react'
+export { renderReact, useLocation } from './react'
