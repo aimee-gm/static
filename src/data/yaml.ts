7643bb7b9641d2ef0cc1e536793a08a9f97eb73d
Aimee Gamble-Milner <aimee-gm@users.noreply.github.com>
2020-02-10 08:56:23 +0000

    feat: migrate to one data loading method

--- src/data/yaml.ts
+++ src/data/yaml.ts
@@ -1,24 +1,5 @@
 import yaml from "js-yaml";
 
-import { glob } from "../utils/glob";
-import { getRelativePath } from "../utils/getRelativePath";
-import { FileData } from "../types";
-
-export function loadYamlData<T extends FileData>(
-  basePath: string,
-  type: string
-): T[] {
-  const raw = glob(`${basePath}/**/*.{yaml,yml}`);
-
-  return raw.map(({ filepath, contents }) => {
-    const parsed = yaml.safeLoad(contents);
-    const relpath = getRelativePath(basePath, filepath);
-
-    return {
-      ...parsed,
-      type,
-      filepath,
-      relpath,
-    };
-  });
+export function parseYaml(contents: string) {
+  return yaml.safeLoad(contents);
 }
